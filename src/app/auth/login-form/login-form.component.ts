32157e0b86ffaf4bfdab1faa2bf54f0beef227ed
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-04-24 19:08:04 +0300

    Add implementation of login.

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 import {LoggerService} from "../../shared/logger/logger.service";
+import {AuthService} from "../services/auth.service";
+import {LoginCredentials} from "../../models/LoginCredentials";
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+    selector: 'app-login-form',
+    templateUrl: './login-form.component.html',
+    styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
 
     loginForm: FormGroup;
 
-    constructor(private logger: LoggerService, public fb: FormBuilder) {
+    constructor(private logger: LoggerService, public fb: FormBuilder, private auth: AuthService) {
     }
 
 
@@ -21,6 +23,15 @@ export class LoginFormComponent implements OnInit {
 
     onSubmit(formData) {
         this.logger.msg(formData, 1);
+        let user: LoginCredentials = new LoginCredentials();
+        user.username = formData.username;
+        user.password = formData.password;
+        this.auth.authenticate(user).subscribe(result => {
+                this.logger.msg(result.headers.get("authorization"), 1);
+            },
+            error => {
+                this.logger.msg(error, 1);
+            });
     }
 
     buildForm(): void {
