a4715516d2407ddc81fbbe1660f0363ec08511a0
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-04-24 16:03:03 +0300

    Add method stub authenticate to AuthService.

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {UserCredentials} from "../../models/UserCredentials";
+import {Observable} from "rxjs";
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+    constructor() {
+    }
+
+    authenticate(user: UserCredentials): Observable<any> {
+        return null;
+    }
 
 }
