376fb1a84a8c77aad8d4100ca971431e9b4b1bbe
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-04-24 19:31:47 +0300

    Add register to AuthService.

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -4,6 +4,7 @@ export const GLOBALS = {
     API: {
         ROOT            : ROOT,
         LOGIN           : `${ROOT}/login`,
+        REGISTER           : `${ROOT}/register`,
     },
     LOCAL_TOKEN_KEY     : 'STORED_TOKEN',
 };
