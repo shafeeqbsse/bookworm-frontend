5861d5f934dee16b9d25751b6c8b515affb2c3e2
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-04-24 19:30:57 +0300

    Add route for registering.

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {LoginPageComponent} from "./login-page/login-page.component";
 import {UserInfoComponent} from "./user-info/user-info.component";
+import {RegisterFormComponent} from "./register-form/register-form.component";
 
 const routes: Routes = [
     {path: 'login', component: LoginPageComponent},
+    {path: 'register', component: RegisterFormComponent},
     {path: 'me', component: UserInfoComponent}
 
 ];
