6e64771d997192b9e926cf4ac406456ba9fe8947
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-04-24 16:29:49 +0300

    Add implementation to AuthService authenticate.

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,15 +1,27 @@
 import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {LoginCredentials} from "../../models/LoginCredentials";
+import {Http, Response} from "@angular/http";
 
 @Injectable()
 export class AuthService {
 
-    constructor() {
+    constructor(private http: Http) {
     }
 
     authenticate(user: LoginCredentials): Observable<any> {
-        return null;
+        let url = "http://localhost:8080/login";
+        return this.http.post(url, user)
+            .map(this.extractData)
+            .catch(this.handleError);    }
+
+
+    private extractData(res: Response) {
+        const body = res.json();
+        return body || { };
     }
 
+    private handleError (error: Response | any) {
+        return Observable.throw(error);
+    }
 }
