09cf5f377a8345c84addb5b5e16004c07e353942
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2017-04-26 21:06:56 +0300

    Add book-list component

--- src/app/books/browse-books.module.ts
+++ src/app/books/browse-books.module.ts
@@ -2,14 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {BrowseBooksPageComponent} from './browse-books-page/browse-books-page.component';
 import {BrowseBooksRoutingModule} from "./browse-books-routing.module";
+import {BookListComponent} from './book-list/book-list.component';
 import {BookService} from "./services/book.service";
 
 @NgModule({
-    imports: [
-        CommonModule,
-        BrowseBooksRoutingModule
-    ],
-    declarations: [BrowseBooksPageComponent],
+  imports: [
+    CommonModule,
+    BrowseBooksRoutingModule
+  ],
+  declarations: [BrowseBooksPageComponent, BookListComponent],
     providers: [BookService]
 })
 export class BrowseBooksModule {
