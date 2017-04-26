95d504a2c577b323eb786d5105b57d2d81ccba8e
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2017-04-26 11:59:35 +0300

    Add BrowseBooksPage-component for browsing books

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import {LoggerService} from "./shared/logger/logger.service";
 import {SharedModule} from "./shared/shared.module";
 import {GLOBALS} from "./globals";
 import {AuthConfig, AuthHttp} from "angular2-jwt";
+import { BrowseBooksPageComponent } from './books/browse-books-page/browse-books-page.component';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 
 
@@ -21,7 +22,8 @@ export function authHttpServiceFactory(http: Http, options: RequestOptions) {
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        BrowseBooksPageComponent
     ],
     imports: [
         BrowserModule,
---
+++ src/app/books/browse-books-page/browse-books-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-browse-books-page',
+  templateUrl: './browse-books-page.component.html',
+  styleUrls: ['./browse-books-page.component.css']
+})
+export class BrowseBooksPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
