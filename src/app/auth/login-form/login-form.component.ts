77d94173b059747c41e72faaa0f4f33abfdddcb7
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-04-27 01:35:23 +0300

    Add redirect after successful log in.

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -3,6 +3,7 @@ import {FormGroup, FormBuilder, Validators} from "@angular/forms";
 import {LoggerService} from "../../shared/logger/logger.service";
 import {AuthService} from "../services/auth.service";
 import {LoginCredentials} from "../../models/LoginCredentials";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-login-form',
@@ -13,9 +14,10 @@ export class LoginFormComponent implements OnInit {
 
     loginForm: FormGroup;
 
-    @Input() isMini:boolean = false;
+    @Input() isMini: boolean = false;
 
-    constructor(private logger: LoggerService, public fb: FormBuilder, private auth: AuthService) {
+    constructor(private logger: LoggerService, public fb: FormBuilder, private auth: AuthService,
+                private router: Router) {
     }
 
 
@@ -30,6 +32,7 @@ export class LoginFormComponent implements OnInit {
         user.password = formData.password;
         this.auth.authenticate(user).subscribe(result => {
                 this.logger.msg(result.headers.get("authorization"), 1);
+                this.router.navigate(['me']);
             },
             error => {
                 this.logger.msg(error, 1);
