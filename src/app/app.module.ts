b60ab4f3dce40a11b531c08227e072a2a9903ab8
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-18 13:06:37 +0300

    Fix app.module after rebase.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpModule, Http, RequestOptions} from '@angular/http';
-
-import {AppComponent} from './app.component';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
+import {FormsModule} from "@angular/forms";
+import {HttpModule, Http, RequestOptions} from "@angular/http";
+import {AppComponent} from "./app.component";
 import {AppRoutingModule} from "./app-routing.module";
 import {AuthModule} from "./auth/auth.module";
 import {LoggerService} from "./shared/logger/logger.service";
 import {SharedModule} from "./shared/shared.module";
 import {GLOBALS} from "./globals";
 import {AuthConfig, AuthHttp} from "angular2-jwt";
-import { BrowseBooksPageComponent } from './books/browse-books-page/browse-books-page.component';
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
 import {BrowseBooksModule} from "./books/browse-books.module";
 
@@ -31,12 +29,11 @@ export function authHttpServiceFactory(http: Http, options: RequestOptions) {
         HttpModule,
         AuthModule.forRoot(),
         SharedModule.forRoot(),
-        AppRoutingModule,
-        NgbModule.forRoot()
+        NgbModule.forRoot(),
         AuthModule,
         SharedModule,
-        AppRoutingModule,
-        BrowseBooksModule
+        BrowseBooksModule,
+        AppRoutingModule
     ],
     providers: [
         LoggerService,
