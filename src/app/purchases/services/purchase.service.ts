d7d8384d89dac8717ad498e79fd471397d7d9bb1
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-18 14:20:06 +0300

    Add getPurchases method to PurchaseService.

--- src/app/purchases/services/purchase.service.ts
+++ src/app/purchases/services/purchase.service.ts
@@ -1,8 +1,18 @@
 import {Injectable} from "@angular/core";
 import {AuthHttp} from "angular2-jwt";
+import {GLOBALS} from "../../globals";
 
 @Injectable()
 export class PurchaseService {
 
-    constructor(private http: AuthHttp) {}
+    constructor(private http: AuthHttp) {
+    }
+
+    getPurchases() {
+        let url: string = GLOBALS.API.ROOT + "/purchases";
+        return this.http.get(url)
+            .map(response => {
+                return response.json();
+            });
+    }
 }
