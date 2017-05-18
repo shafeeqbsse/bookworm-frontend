fc54033508f04de1fbdebbe13e91ae902e47c021
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-18 16:32:31 +0300

    Add method addToCart(book) to ShoppingCartService.

--- src/app/shared/services/shopping-cart.service.ts
+++ src/app/shared/services/shopping-cart.service.ts
@@ -1,8 +1,21 @@
 import {Injectable} from "@angular/core";
+import {Book} from "../../models/Book";
+import {GLOBALS} from "../../globals";
 
 @Injectable()
 export class ShoppingCartService {
 
-  constructor() { }
+    private cart:Array<Book>;
+
+    constructor() {
+        this.cart = [];
+    }
+
+    addToCart(book: Book) {
+        const text:string  = window.localStorage.getItem(GLOBALS.LOCAL_CART_KEY);
+        this.cart = JSON.parse(text);
+        this.cart.push(book);
+        window.localStorage.setItem(GLOBALS.LOCAL_CART_KEY, JSON.stringify(this.cart));
+    }
 
 }
