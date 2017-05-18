bdc17d2402cc42aa2a1391a3845e1f3bef9d5698
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-18 13:51:21 +0300

    Add saveReviewForBook method to ReviewService.

--- src/app/reviews/services/review.service.ts
+++ src/app/reviews/services/review.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from "@angular/core";
 import {AuthHttp} from "angular2-jwt";
 import {GLOBALS} from "../../globals";
+import {Review} from "../../models/Review";
+import {Headers} from "@angular/http";
 
 @Injectable()
 export class ReviewService {
@@ -9,8 +11,18 @@ export class ReviewService {
     }
 
     getReviewsForBook(bookId: number) {
-        let url: string = GLOBALS.API.ROOT + "/books/" + bookId + "/reviews";
+        const url: string = GLOBALS.API.ROOT + "/books/" + bookId + "/reviews";
         return this.http.get(url)
+            .map(response => {
+                return response.json();
+            });
+    }
+
+    saveReviewForBook(bookId:number, review:Review) {
+        const url: string = GLOBALS.API.ROOT + "/books/" + bookId + "/reviews";
+        const headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this.http.post(url, review)
             .map(response => {
                 return response.json();
             });
