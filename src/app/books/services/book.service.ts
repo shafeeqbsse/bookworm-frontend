2877e852a61b25a5182b204cfe72e729cf4f9fb0
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-18 12:32:48 +0300

    Add method saveBook to BookService.

--- src/app/books/services/book.service.ts
+++ src/app/books/services/book.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from "@angular/core";
 import {AuthHttp} from "angular2-jwt";
 import {GLOBALS} from "../../globals";
+import {Book} from "../../models/Book";
+import {Headers} from "@angular/http";
 
 @Injectable()
 export class BookService {
@@ -19,6 +21,16 @@ export class BookService {
     getBook(id: number) {
         let url: string = GLOBALS.API.ROOT + "/books/" + id;
         return this.http.get(url)
+            .map(response => {
+                return response.json();
+            });
+    }
+
+    saveBook(book: Book) {
+        const url: string = GLOBALS.API.ROOT + "/books/";
+        const headers = new Headers();
+        headers.append('Content-Type','application/json');
+        return this.http.post(url, book)
             .map(response => {
                 return response.json();
             });
