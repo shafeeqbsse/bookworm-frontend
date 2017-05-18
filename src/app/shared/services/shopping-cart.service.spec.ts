75a28c3b57fd7ae04862a75a83e0e2075e2648ea
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-18 16:11:17 +0300

    Add ShoppingCartService to shared.

---
+++ src/app/shared/services/shopping-cart.service.spec.ts
@@ -0,0 +1,14 @@
+import {TestBed, inject} from "@angular/core/testing";
+import {ShoppingCartService} from "./shopping-cart.service";
+
+describe('ShoppingCartService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ShoppingCartService]
+    });
+  });
+
+  it('should ...', inject([ShoppingCartService], (service: ShoppingCartService) => {
+    expect(service).toBeTruthy();
+  }));
+});
