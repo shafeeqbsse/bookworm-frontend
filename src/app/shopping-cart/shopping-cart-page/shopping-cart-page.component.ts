6679ea650faeb618f3df52d18030b726ae0997b6
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-19 15:28:38 +0300

    Add ShoppingCartPage component.

---
+++ src/app/shopping-cart/shopping-cart-page/shopping-cart-page.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+  selector: 'app-shopping-cart-page',
+  templateUrl: './shopping-cart-page.component.html',
+  styleUrls: ['./shopping-cart-page.component.css']
+})
+export class ShoppingCartPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/shopping-cart/shopping-cart.module.ts
+++ src/app/shopping-cart/shopping-cart.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {ShoppingCartRoutingModule} from "./shopping-cart-routing.module";
+import {ShoppingCartPageComponent} from "./shopping-cart-page/shopping-cart-page.component";
 
 @NgModule({
   imports: [
     CommonModule,
     ShoppingCartRoutingModule
   ],
-  declarations: []
+  declarations: [ShoppingCartPageComponent]
 })
 export class ShoppingCartModule { }
