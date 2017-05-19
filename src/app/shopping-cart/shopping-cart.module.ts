0302490d6468d4c7c5b1fd867e2121550cd8a772
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-19 15:27:43 +0300

    Add shopping-cart module.

---
+++ src/app/shopping-cart/shopping-cart.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {ShoppingCartRoutingModule} from "./shopping-cart-routing.module";
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ShoppingCartRoutingModule
+  ],
+  declarations: []
+})
+export class ShoppingCartModule { }
