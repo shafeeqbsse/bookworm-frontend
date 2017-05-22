94ee5db2b8acfc6c49c9ba1bdbaf1e46e1cb9a2c
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-22 14:41:35 +0300

    Add publisher to add-book-form.

---
+++ src/app/admin/add-book-form/publisher-selector/publisher-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-publisher-selector',
+  templateUrl: './publisher-selector.component.html',
+  styleUrls: ['./publisher-selector.component.css']
+})
+export class PublisherSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -5,6 +5,7 @@ import {AdminRoutingModule} from './admin-routing.module';
 import {AdminPageComponent} from './admin-page/admin-page.component';
 import { AddBookFormComponent } from './add-book-form/add-book-form.component';
 import {ReactiveFormsModule} from "@angular/forms";
+import { PublisherSelectorComponent } from './add-book-form/publisher-selector/publisher-selector.component';
 
 @NgModule({
     imports: [
@@ -12,7 +13,7 @@ import {ReactiveFormsModule} from "@angular/forms";
         ReactiveFormsModule,
         AdminRoutingModule
     ],
-    declarations: [AdminPageComponent, AddBookFormComponent]
+    declarations: [AdminPageComponent, AddBookFormComponent, PublisherSelectorComponent]
 })
 export class AdminModule {
 }
