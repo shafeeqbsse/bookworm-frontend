6727134d2d0459637c9ee663d46a72eeced7d7eb
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-22 14:07:21 +0300

    Add description to add-book-form.

--- src/app/admin/add-book-form/add-book-form.component.ts
+++ src/app/admin/add-book-form/add-book-form.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormGroup, Validators, FormBuilder} from "@angular/forms";
+import {Book} from "../../models/Book";
+import {BookService} from "../../books/services/book.service";
 
 @Component({
     selector: 'app-add-book-form',
@@ -8,9 +10,12 @@ import {FormGroup, Validators, FormBuilder} from "@angular/forms";
 })
 export class AddBookFormComponent implements OnInit {
 
-    bookForm:FormGroup;
+    bookForm: FormGroup;
 
-    constructor(public fb: FormBuilder) {
+    public addBookFailed: boolean;
+
+    constructor(public fb: FormBuilder, private bookService: BookService) {
+        this.addBookFailed = false;
     }
 
     ngOnInit() {
@@ -19,8 +24,24 @@ export class AddBookFormComponent implements OnInit {
 
     buildForm(): void {
         this.bookForm = this.fb.group({
-            title: ['', Validators.compose([Validators.required])]
+            title: ['', Validators.compose([Validators.required])],
+            description: ['', Validators.compose([Validators.required])],
+            genre: ['', Validators.compose([Validators.required])]
         });
+    }
+
+    onSubmit(formData) {
+        let book: Book = new Book();
+        book.title = formData.title;
+        book.description = formData.description;
+        this.bookService.saveBook(book).subscribe(
+            result => {
+                console.debug("Saved book", result);
+            },
+            error => {
+                console.error("Error saving book", error);
+            }
+        );
     }
 
 }
