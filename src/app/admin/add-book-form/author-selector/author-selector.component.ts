05a429aaf3b4d6b3a6ba025a580d34feedb174d5
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-22 17:48:49 +0300

    Add implement AuthorSelectorComponent typeahead.

--- src/app/admin/add-book-form/author-selector/author-selector.component.ts
+++ src/app/admin/add-book-form/author-selector/author-selector.component.ts
@@ -1,4 +1,7 @@
 import {Component, OnInit} from "@angular/core";
+import {Author} from "../../../models/Author";
+import {Observable} from "rxjs";
+import {AuthorService} from "../../services/author.service";
 
 @Component({
     selector: 'app-author-selector',
@@ -7,10 +10,45 @@ import {Component, OnInit} from "@angular/core";
 })
 export class AuthorSelectorComponent implements OnInit {
 
-    constructor() {
+    public authors: Array<Author>;
+
+    public selectedAuthor: Author;
+
+    searching: boolean = false;
+    searchFailed: boolean = false;
+
+
+    constructor(private authorService: AuthorService) {
+        this.authors = [];
     }
 
     ngOnInit() {
     }
 
+    search = (text$: Observable<string>) => {
+        return text$
+            .debounceTime(300)
+            .distinctUntilChanged()
+            .do(() => this.searching = true)
+            .switchMap(term =>
+                this.authorService.search(term)
+                    .do(() => this.searchFailed = false)
+                    .catch(() => {
+                        this.searchFailed = true;
+                        return Observable.of([]);
+                    }))
+            .do(() => this.searching = false);
+    };
+
+    formatter = (a: Author) => {
+        return a.firstName + " " + a.lastName;
+    };
+
+    addSelected() {
+        if (this.selectedAuthor.authorId) {
+            console.debug("Selected Author:", this.selectedAuthor);
+        } else {
+            console.debug("Selected Author:", "None yet");
+        }
+    }
 }
