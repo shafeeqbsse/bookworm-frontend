9f75bdbd7f552e02d638e4c0f9160a8bd7431fbf
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-24 17:34:48 +0300

    Add provider for ReviewService.

--- src/app/reviews/reviews.module.ts
+++ src/app/reviews/reviews.module.ts
@@ -1,13 +1,17 @@
 import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {AddReviewFormComponent} from "./add-review-form/add-review-form.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {ReviewService} from "./services/review.service";
 
 @NgModule({
     imports: [
-        CommonModule
+        CommonModule,
+        ReactiveFormsModule
     ],
     declarations: [AddReviewFormComponent],
-    exports: [AddReviewFormComponent]
+    exports: [AddReviewFormComponent],
+    providers: [ReviewService]
 })
 export class ReviewsModule {
 }
