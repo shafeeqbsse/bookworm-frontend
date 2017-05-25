1165da4d3317d9d5dbd07e01b13835c2b2583efe
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-25 22:29:54 +0300

    Add reviews to book-details-page.

--- src/app/books/browse-books.module.ts
+++ src/app/books/browse-books.module.ts
@@ -4,17 +4,19 @@ import {BrowseBooksPageComponent} from "./browse-books-page/browse-books-page.co
 import {BrowseBooksRoutingModule} from "./browse-books-routing.module";
 import {BookService} from "./services/book.service";
 import {GenreService} from "./services/genre.service";
-import { GenreListComponent } from './genre-list/genre-list.component';
+import {GenreListComponent} from "./genre-list/genre-list.component";
 import {ReactiveFormsModule, FormsModule} from "@angular/forms";
 import {SharedModule} from "../shared/shared.module";
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import { BookDetailsComponent } from './book-details/book-details.component';
+import {BookDetailsComponent} from "./book-details/book-details.component";
+import {ReviewsModule} from "../reviews/reviews.module";
 
 @NgModule({
     imports: [
         CommonModule,
         BrowseBooksRoutingModule,
         ReactiveFormsModule,
+        ReviewsModule,
         FormsModule,
         SharedModule,
         NgbModule
--- src/app/reviews/review-tabbed-container/review-tabbed-container.component.ts
+++ src/app/reviews/review-tabbed-container/review-tabbed-container.component.ts
@@ -7,7 +7,6 @@ import {Component, OnInit, Input} from "@angular/core";
 })
 export class ReviewTabbedContainerComponent implements OnInit {
 
-
     tab: string = 'review';
 
     @Input() bookId;
