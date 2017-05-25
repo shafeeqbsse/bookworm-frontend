a9f8525fe4d592d54da90156c3d640c51da5ced3
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2017-05-25 18:35:02 +0300

    Fix routes and add link to details-page

--- src/app/books/browse-books-routing.module.ts
+++ src/app/books/browse-books-routing.module.ts
@@ -1,10 +1,11 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {BrowseBooksPageComponent} from "./browse-books-page/browse-books-page.component";
+import {BookDetailsComponent} from "./book-details/book-details.component";
 
 const routes: Routes = [
     {path: 'browse', component: BrowseBooksPageComponent},
-    {path: 'book/:bookId'}
+    {path: 'book/:bookId', component: BookDetailsComponent}
 ];
 
 @NgModule({
