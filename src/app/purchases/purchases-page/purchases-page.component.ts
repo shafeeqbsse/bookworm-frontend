d31c8723bde3976f1bd7c467837499d8c6ace7a8
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-25 22:55:49 +0300

    Add stub purchases-page.component.

---
+++ src/app/purchases/purchases-page/purchases-page.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+    selector: 'app-purchases-page',
+    templateUrl: './purchases-page.component.html',
+    styleUrls: ['./purchases-page.component.css']
+})
+export class PurchasesPageComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
--- src/app/purchases/purchases.module.ts
+++ src/app/purchases/purchases.module.ts
@@ -2,13 +2,14 @@ import {NgModule} from "@angular/core";
 import {CommonModule} from "@angular/common";
 import {PurchasesRoutingModule} from "./purchases-routing.module";
 import {PurchaseService} from "./services/purchase.service";
+import {PurchasesPageComponent} from "./purchases-page/purchases-page.component";
 
 @NgModule({
     imports: [
         CommonModule,
         PurchasesRoutingModule
     ],
-    declarations: [],
+    declarations: [PurchasesPageComponent],
     providers: [PurchaseService]
 })
 export class PurchasesModule {
