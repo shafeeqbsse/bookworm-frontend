c5a5a8f5adced7d7a572a82490fc1c37b7577b00
Toni Seppäläinen <toni.seppalainen@cs.tamk.fi>
2017-05-25 23:42:11 +0300

    Add contents for purchases-page.component.

--- src/app/purchases/purchases-page/purchases-page.component.ts
+++ src/app/purchases/purchases-page/purchases-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {PurchaseService} from "../services/purchase.service";
 
 @Component({
     selector: 'app-purchases-page',
@@ -6,11 +7,37 @@ import {Component, OnInit} from "@angular/core";
     styleUrls: ['./purchases-page.component.css']
 })
 export class PurchasesPageComponent implements OnInit {
+    purchases: Array<Purchase>;
 
-    constructor() {
+    constructor(private service: PurchaseService) {
     }
 
     ngOnInit() {
+        this.service.getPurchases().subscribe(result => {
+            this.purchases = result.map(p => {
+                let purchase: Purchase;
+                purchase = p;
+                return purchase;
+            });
+            console.debug("Purchases: ", this.purchases);
+        }, error => {
+            console.error("Error getting purhases:", error);
+        })
     }
+}
 
+class Purchase {
+    purchaseId: number;
+    member: Member;
+    book: PurchaseBook;
+    amount: number;
+}
+class Member {
+    memberId: number;
+    email: string;
+    username: string;
+}
+class PurchaseBook {
+    bookId: number;
+    title: string;
 }
--- src/app/purchases/purchases-routing.module.ts
+++ src/app/purchases/purchases-routing.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from "@angular/core";
 import {Routes, RouterModule} from "@angular/router";
+import {AdminGuard} from "../auth/guards/AdminGuard";
+import {PurchasesPageComponent} from "./purchases-page/purchases-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    {path: "purchases", canActivate: [AdminGuard], component: PurchasesPageComponent}
+];
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
