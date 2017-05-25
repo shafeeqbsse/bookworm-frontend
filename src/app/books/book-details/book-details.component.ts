259d69593ddce55f7b9f03cbc61076b32d4ddeed
Valtteri Poutanen <valtteri.poutanen@hotmail.com>
2017-05-26 02:10:37 +0300

    Add function to subscribe for notifications

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -49,6 +49,11 @@ export class BookDetailsComponent implements OnInit {
     }
 
     notify() {
-        console.log("notify");
+        this.bookService.addNotificationForRestock(this.book.bookId).subscribe(response => {
+                console.log("Subscribed!");
+            },
+            error => {
+                console.error("Subscribe failed:", error);
+            });
     }
 }
